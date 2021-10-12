baf54fff61b89f5435dbbe5a4a478f1a224b427c
Ziyad Edher <ziyad.edher@gmail.com>
2021-10-11 21:28:30 -0700

    Fix small errors due to incorrect tag usage (#14)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,6 +62,7 @@ const Index: NextPage = () => (
         content="Ziyad Edher is a software engineer interested in AI/ML, entrepreneurship, security, and infrastructure."
       />
     </Head>
+
     <div className="container flex flex-col justify-center items-center mx-auto">
       <header className="flex flex-col py-12 text-center">
         <div className="text-2xl font-light uppercase hover:opacity-70 transition-all">
@@ -130,47 +131,45 @@ const Index: NextPage = () => (
             nature. Hit me up if you wanna talk about that stuff or anything
             else, it&apos;s not like I have anything better to do.
           </p>
-          <p>
-            Some maybe cool things I did in the past:
-            <ul>
-              <li>
-                Building ML and data infrastructure at{" "}
-                <TextLink href="https://www.citadelsecurities.com/" isExternal>
-                  Citadel Securities
-                </TextLink>
-                .
-              </li>
-              <li>
-                Designing software that protects{" "}
-                <TextLink href="https://asana.com?noredirect" isExternal>
-                  Asana
-                </TextLink>{" "}
-                users against scammers and bots.
-              </li>
-              <li>
-                Interning at{" "}
-                <TextLink href="https://google.com" isExternal>
-                  Google
-                </TextLink>{" "}
-                (twice) working on{" "}
-                <TextLink href="https://fi.google.com/" isExternal>
-                  communications
-                </TextLink>{" "}
-                and{" "}
-                <TextLink href="https://cloud.google.com/" isExternal>
-                  security
-                </TextLink>
-                .
-              </li>
-              <li>
-                Teaching cars how to drive at{" "}
-                <TextLink href="https://www.autodrive.utoronto.ca/" isExternal>
-                  aUToronto
-                </TextLink>
-                .
-              </li>
-            </ul>
-          </p>
+          <p className="mb-1">Some maybe cool things I did in the past:</p>
+          <ul className="mt-0">
+            <li>
+              Building ML and data infrastructure at{" "}
+              <TextLink href="https://www.citadelsecurities.com/" isExternal>
+                Citadel Securities
+              </TextLink>
+              .
+            </li>
+            <li>
+              Designing software that protects{" "}
+              <TextLink href="https://asana.com?noredirect" isExternal>
+                Asana
+              </TextLink>{" "}
+              users against scammers and bots.
+            </li>
+            <li>
+              Interning at{" "}
+              <TextLink href="https://google.com" isExternal>
+                Google
+              </TextLink>{" "}
+              (twice) working on{" "}
+              <TextLink href="https://fi.google.com/" isExternal>
+                communications
+              </TextLink>{" "}
+              and{" "}
+              <TextLink href="https://cloud.google.com/" isExternal>
+                security
+              </TextLink>
+              .
+            </li>
+            <li>
+              Teaching cars how to drive at{" "}
+              <TextLink href="https://www.autodrive.utoronto.ca/" isExternal>
+                aUToronto
+              </TextLink>
+              .
+            </li>
+          </ul>
           <p>
             There used to be more content here but I got bored of keeping it up
             to date so go{" "}
