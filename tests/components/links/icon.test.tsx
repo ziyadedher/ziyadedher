a894c68655928b6e17cba9baa629b4f6e26c825f
Ziyad Edher <ziyad.edher@gmail.com>
2021-10-24 18:56:33 -0700

    Write tests for link components

---
+++ tests/components/links/icon.test.tsx
@@ -0,0 +1,58 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// eslint-disable-next-line import/no-unassigned-import -- @testing-library/jest-dom doesn't need to be bound.
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { PhosphorLogo } from "phosphor-react";
+
+import IconLink from "../../../src/components/links/icon";
+
+describe("icon link", () => {
+  test("renders link child", () => {
+    const tree = render(
+      <IconLink href="https://example.com">
+        <PhosphorLogo data-testid="logo" size={24} weight="light" />
+      </IconLink>
+    );
+    expect(tree.getByTestId("logo")).toBeInTheDocument();
+  });
+
+  test("has correct href", () => {
+    const tree = render(
+      <IconLink href="https://example.com">
+        <PhosphorLogo data-testid="logo" size={24} weight="light" />
+      </IconLink>
+    );
+    expect(tree.getByTestId("logo").parentElement).toHaveAttribute(
+      "href",
+      "https://example.com"
+    );
+  });
+
+  describe("external", () => {
+    test("does not set target when not external", () => {
+      const tree = render(
+        <IconLink href="https://example.com" isExternal={false}>
+          <PhosphorLogo data-testid="logo" size={24} weight="light" />
+        </IconLink>
+      );
+      expect(tree.getByTestId("logo").parentElement).not.toHaveAttribute(
+        "target"
+      );
+    });
+
+    test("sets target to blank when external", () => {
+      const tree = render(
+        <IconLink href="https://example.com" isExternal>
+          <PhosphorLogo data-testid="logo" size={24} weight="light" />
+        </IconLink>
+      );
+      expect(tree.getByTestId("logo").parentElement).toHaveAttribute("target");
+      expect(
+        tree.getByTestId("logo").parentElement?.getAttribute("target")
+      ).toBe("_blank");
+    });
+  });
+});
