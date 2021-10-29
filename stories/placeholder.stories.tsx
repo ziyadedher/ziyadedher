e05b7e6a3aa52f66d6e5e0dec9b5155d2ce8c1a2
Ziyad Edher <ziyad.edher@gmail.com>
2021-10-28 22:50:58 -0700

    Clean up stories (#61)

--- stories/placeholder.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { Meta, Story } from "@storybook/react";
-
-const META: Meta = {
-  title: "Pages/Placeholder",
-};
-
-export const Placeholder: Story = () => <p>Hello, world!</p>;
-
-export default META;
