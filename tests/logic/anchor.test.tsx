4acac8d13d486cdf5cd7930d614233175623e96a
Ziyad Edher <ziyad.edher@gmail.com>
2021-10-29 22:18:40 -0700

    Fix some test terminology and storybook names (#68)

--- tests/logic/anchor.test.tsx
+++ tests/logic/anchor.test.tsx
@@ -15,45 +15,47 @@ import Anchor from "../../src/logic/anchor";
  */
 describe("anchor", () => {
   test("default render matches snapshot", () => {
-    const tree = render(
+    const result = render(
       <Anchor href="https://example.org/" label="example">
         Hello, world!
       </Anchor>
     );
-    expect(tree).toMatchSnapshot();
+    expect(result).toMatchSnapshot();
   });
 
   test("should render anchor tag", () => {
-    const tree = render(<Anchor href="https://example.org/" />);
-    expect(tree.getByRole("link")).toBeInTheDocument();
+    const result = render(<Anchor href="https://example.org/" />);
+    expect(result.getByRole("link")).toBeInTheDocument();
   });
 
   test("href should be reflected in anchor tag", () => {
-    const tree = render(<Anchor href="https://example.org/" />);
-    expect(tree.getByRole("link")).toHaveAttribute(
+    const result = render(<Anchor href="https://example.org/" />);
+    expect(result.getByRole("link")).toHaveAttribute(
       "href",
       "https://example.org/"
     );
   });
 
   test("label should be reflected in anchor tag", () => {
-    const tree = render(<Anchor href="https://example.org/" label="example" />);
-    expect(tree.getByRole("link")).toHaveAttribute("aria-label", "example");
+    const result = render(
+      <Anchor href="https://example.org/" label="example" />
+    );
+    expect(result.getByRole("link")).toHaveAttribute("aria-label", "example");
   });
 
   describe("opening in new tab", () => {
     test("not should open in new tab means no target", () => {
-      const tree = render(
+      const result = render(
         <Anchor href="https://example.org/" shouldOpenInNewPage={false} />
       );
-      expect(tree.getByRole("link")).not.toHaveAttribute("target");
+      expect(result.getByRole("link")).not.toHaveAttribute("target");
     });
 
     test("should open in new tab means target and rel", () => {
-      const tree = render(
+      const result = render(
         <Anchor href="https://example.org/" shouldOpenInNewPage />
       );
-      const anchorElement = tree.getByRole("link");
+      const anchorElement = result.getByRole("link");
       expect(anchorElement).toHaveAttribute("target", "_blank");
       expect(anchorElement).toHaveAttribute("rel", "noreferrer");
     });
@@ -61,8 +63,8 @@ describe("anchor", () => {
 
   describe("children", () => {
     test("should be forwarded to anchor tag", () => {
-      const tree = render(<Anchor href="https://example.org/">test</Anchor>);
-      expect(tree.getByText("test")).toBeInTheDocument();
+      const result = render(<Anchor href="https://example.org/">test</Anchor>);
+      expect(result.getByText("test")).toBeInTheDocument();
     });
   });
 });
