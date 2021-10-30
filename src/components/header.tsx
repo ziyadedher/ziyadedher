a45292289d09880a28f7c7c4aa41d7e550fbd7d0
Ziyad Edher <ziyad.edher@gmail.com>
2021-10-29 21:47:20 -0700

    Pull out some common components from the homepage (#66)

---
+++ src/components/header.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import {
+  FacebookLogo,
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  SpotifyLogo,
+  TwitterLogo,
+} from "phosphor-react";
+import React from "react";
+
+import Anchor from "../logic/anchor";
+
+import IconLink from "./links/icon";
+
+const Header: React.FunctionComponent = () => (
+  <header className="flex flex-col items-center py-12 text-center">
+    <div className="text-2xl font-light uppercase hover:opacity-70 transition-all">
+      <Link href="/" passHref>
+        <Anchor>Ziyad Edher</Anchor>
+      </Link>
+    </div>
+
+    <div className="flex flex-row pt-2 space-x-2">
+      <IconLink
+        href="https://twitter.com/ziyadedher"
+        label="Twitter"
+        isExternal
+      >
+        <TwitterLogo size={24} weight="light" />
+      </IconLink>
+      <IconLink
+        href="https://linkedin.com/in/ziyad-edher"
+        label="LinkedIn"
+        isExternal
+      >
+        <LinkedinLogo size={24} weight="light" />
+      </IconLink>
+      <IconLink href="https://github.com/ziyadedher" label="GitHub" isExternal>
+        <GithubLogo size={24} weight="light" />
+      </IconLink>
+      <IconLink
+        href="https://open.spotify.com/user/c9n8y6h3ssu8hg811rfqt4tvrr"
+        label="Spotify"
+        isExternal
+      >
+        <SpotifyLogo size={24} weight="light" />
+      </IconLink>
+      <IconLink
+        href="https://facebook.com/ziyadedher"
+        label="Facebook"
+        isExternal
+      >
+        <FacebookLogo size={24} weight="light" />
+      </IconLink>
+      <IconLink
+        href="https://instagram.com/ziyadedher"
+        label="Instagram"
+        isExternal
+      >
+        <InstagramLogo size={24} weight="light" />
+      </IconLink>
+    </div>
+  </header>
+);
+
+export default Header;
