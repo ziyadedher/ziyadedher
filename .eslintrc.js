a40e96ad12173a8a5a9315c1efe27f8b6aedfb72
Ziyad Edher <ziyad.edher@gmail.com>
2021-11-03 18:56:12 -0700

    Upgrade @ziyadedher/eslint-config (#98)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,5 @@
 /* eslint-disable import/unambiguous -- exception for ESLint configuration file. */
 
 module.exports = {
-  parserOptions: {
-    project: "./tsconfig.json",
-    sourceType: "module",
-    ecmaVersion: 2021,
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   extends: ["@ziyadedher"],
-  rules: {
-    "react/destructuring-assignment": ["error", "always"],
-    "react/require-default-props": ["off"],
-    "node/no-unpublished-import": ["off"],
-  },
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
-    },
-    "import/resolver": {
-      typescript: {
-        project: "./tsconfig.json",
-        alwaysTryTypes: true,
-      },
-    },
-    node: {
-      tryExtensions: [".json", ".js", ".jsx", ".ts", ".tsx", ".d.ts", ".d.ts"],
-    },
-    react: {
-      version: "detect",
-    },
-  },
 };
