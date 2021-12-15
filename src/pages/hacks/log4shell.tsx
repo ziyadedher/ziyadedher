9022b03817e7588844fe87501bfc30b816bc3c19
Ziyad Edher <ziyad.edher@gmail.com>
2021-12-15 14:57:30 -0800

    Fix log4shell page for mobile (#192)

--- src/pages/hacks/log4shell.tsx
+++ src/pages/hacks/log4shell.tsx
@@ -84,68 +84,64 @@ const Index: NextPage = () => (
       />
     </Head>
 
-    <div className="flex flex-col min-h-screen text-green-500 bg-gray-900">
+    <div className="min-h-screen text-green-500 bg-gray-900">
       <PageContainer>
         <div className="my-8 space-y-4">
           <Header />
           <Navbar currentPage={null} />
         </div>
 
-        <div className="flex flex-col gap-4 items-center mt-12">
-          <h1 className="text-xl">Log4Shell Public Bug Bounty Specials</h1>
-          <p className="max-w-xl text-sm text-center">
-            Log4Shell (
-            <Link
-              href="https://nvd.nist.gov/vuln/detail/CVE-2021-44228"
-              passHref
-            >
-              <Anchor shouldOpenInNewPage>CVE-2021-44228</Anchor>
-            </Link>
-            ) is an RCE 0day in Log4j, a popular Java logging framework. Many
-            companies have put together bug bounty specials for anyone who can
-            demonstrate that they are still vulnerable to Log4Shell.
-          </p>
+        <main className="flex flex-row justify-center py-8 space-x-8 w-full max-w-3xl">
+          <div className="flex flex-col flex-1 gap-2 items-center px-6 xl:mx-0 w-full text-sm font-light text-center">
+            <h1 className="text-xl">Log4Shell Public Bug Bounty Specials</h1>
+            <p className="max-w-xl text-sm">
+              Log4Shell (
+              <Link
+                href="https://nvd.nist.gov/vuln/detail/CVE-2021-44228"
+                passHref
+              >
+                <Anchor shouldOpenInNewPage>CVE-2021-44228</Anchor>
+              </Link>
+              ) is an RCE 0day in Log4j, a popular Java logging framework. Many
+              companies have put together bug bounty specials for anyone who can
+              demonstrate that they are still vulnerable to Log4Shell.
+            </p>
 
-          <div className="flex flex-col my-12">
-            <div className="overflow-x-auto -my-2 -mx-8">
-              <div className="inline-block py-2 px-8 min-w-full align-middle">
-                <div className="overflow-hidden rounded-lg">
-                  <table className="min-w-full">
-                    <thead className="border-b border-green-500">
-                      <tr>
-                        <TableHeaderItem headerName="Company" />
-                        <TableHeaderItem headerName="Bug Bounty" />
-                        <TableHeaderItem headerName="Amount (USD)" />
-                        <TableHeaderItem headerName="Dates" />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {HIT_LIST.map((item, i) => (
-                        <TableRow
-                          key={item.companyName}
-                          item={item}
-                          isOdd={i % 2 === 1}
-                        />
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
+            <div className="flex overflow-x-auto flex-col flex-1 my-8 max-w-full text-left">
+              <table className="table-auto">
+                <thead className="border-b border-green-500">
+                  <tr>
+                    <TableHeaderItem headerName="Company" />
+                    <TableHeaderItem headerName="Bug Bounty" />
+                    <TableHeaderItem headerName="Amount (USD)" />
+                    <TableHeaderItem headerName="Dates" />
+                  </tr>
+                </thead>
+                <tbody>
+                  {HIT_LIST.map((item, i) => (
+                    <TableRow
+                      key={item.companyName}
+                      item={item}
+                      isOdd={i % 2 === 1}
+                    />
+                  ))}
+                </tbody>
+              </table>
             </div>
-          </div>
 
-          <h2 className="opacity-60 hover:opacity-90 active:opacity-100 transition-all">
-            <Link href="/api/hacks/log4shell" passHref>
-              <Anchor shouldOpenInNewPage>
-                Click here for the data in the table as JSON.
-              </Anchor>
-            </Link>
-          </h2>
-          <p className="max-w-md text-sm text-center opacity-40">
-            If you have any questions or see anything missing from this list,
-            please contact me on Twitter or however else you find me.
-          </p>
-        </div>
+            <h2 className="opacity-60 hover:opacity-90 active:opacity-100 transition-all">
+              <Link href="/api/hacks/log4shell" passHref>
+                <Anchor shouldOpenInNewPage>
+                  Click here for the data in the table as JSON.
+                </Anchor>
+              </Link>
+            </h2>
+            <p className="max-w-md text-sm opacity-40">
+              If you have any questions or see anything missing from this list,
+              please contact me on Twitter or however else you find me.
+            </p>
+          </div>
+        </main>
       </PageContainer>
     </div>
   </>
