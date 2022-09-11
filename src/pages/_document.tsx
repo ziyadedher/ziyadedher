7ada9ee859f7a569fbf87dfe4c96327c0bfb24cb
Ziyad Edher <ziyad.edher@gmail.com>
2022-09-10 23:04:59 -0700

    Build out ottawexit page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,10 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
 
-import { getStorageURI } from "../utils/storage";
-
 // eslint-disable-next-line @typescript-eslint/no-shadow -- Next.js pattern.
 const Document: React.FunctionComponent = () => (
   <Html lang="en">
-    <Head>
-      <meta charSet="utf-8" />
-      <link
-        rel="shortcut icon"
-        type="image/png"
-        href={getStorageURI("brain.png")}
-      />
-    </Head>
+    <Head />
     <body>
       <Main />
       <NextScript />
