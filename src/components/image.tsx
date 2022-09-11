45b370ff22fe64efc1431978268dfcef1b4bd663
Ziyad Edher <ziyad.edher@gmail.com>
2022-09-11 00:07:44 -0700

    Build out Ottawexit page (#499)

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -3,23 +3,29 @@ import React from "react";
 
 import type { ImageWithBlur } from "../logic/image_with_blur";
 
+type LayoutValue = "fill" | "fixed" | "intrinsic" | "responsive";
 interface ImageProps {
   readonly alt: string;
   readonly image: ImageWithBlur;
+  readonly shouldUseBlur?: boolean;
+  readonly layout?: LayoutValue;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-shadow -- wrapper around Next.js Image
 const Image: React.FunctionComponent<ImageProps> = ({
   alt,
   image,
+  shouldUseBlur = true,
+  layout,
 }: ImageProps) => (
   <NextImage
     alt={alt}
     src={image.url}
     width={image.width}
     height={image.height}
-    placeholder="blur"
+    placeholder={shouldUseBlur ? "blur" : "empty"}
     blurDataURL={image.blurData}
+    layout={layout}
   />
 );
 
