ec54289967c276a01cd3743cc06287726bc96649
Ziyad Edher <ziyad.edher@gmail.com>
2022-12-19 17:51:08 -0800

    Fix more

--- .jest/setup_environment.ts
+++ .jest/setup_environment.ts
@@ -2,3 +2,5 @@ import { loadEnvConfig } from "@next/env";
 
 // eslint-disable-next-line jest/require-hook -- loading environment variables.
 loadEnvConfig(process.cwd());
+
+jest.mock("next/router", () => require("next-router-mock"));
